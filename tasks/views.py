3280078f306148b64fd53dd3b3527d088583949b
Bilal Leadbitter <bilal.leadbitter@gmail.com>
2021-04-09 16:05:47 +0100

    Implemented first template and resolved issue with it not appearing on web page

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import *
 # Create your views here.
+
 def index(request):
-    return render(request, 'tasks/lists.html')
+    tasks = Task.objects.all()
+
+    context = {"tasks":tasks}
+    print(repr(context))
+    return render(request, 'tasks/lists.html', context)
